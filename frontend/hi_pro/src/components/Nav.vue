f687770c106dafda1567b8ce433c3211627cd3ec
이하연 <dltmddms3711@naver.com>
2020-05-14 16:41:53 +0900

    S02P31C105-34[+]feat: 관리자 웹 프로젝트 생성 navigation bar

---
+++ frontend/hi_pro/src/components/Nav.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="nav">
+    <div class="menu">
+      <img src="../assets/header-logo.jpg" />
+    </div>
+    <div class="menu">
+      <v-router>교육생관리</v-router>
+    </div>
+    <div class="menu">출결관리</div>
+    <div class="menu">공지사항관리</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.menu {
+  float: left;
+  width: 25%;
+  font-size: 20px;
+  text-align: center;
+  line-height: 120px;
+}
+.menu img {
+  vertical-align: middle;
+}
+</style>
---
+++ frontend/hi_pro/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+// const rejectAuthUser =(to,from,next)=>{
+//   if(StorageEvent.getters.isAuthenticated ===false){
+//     alert("로그인 해야 사용하실 수 있습니다.")
+//     next("/")
+//   }else{
+//     next()
+//   }
+// }
+
+const routes = [
+
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
