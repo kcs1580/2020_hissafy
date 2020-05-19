486c56d7f7172cd19d3a9e135875347bcb761878
이하연 <dltmddms3711@naver.com>
2020-05-19 17:04:44 +0900

    S02P31C105-38[+]fix: ProController Controller 어노테이션 추가

--- backend/hissafy/src/main/java/com/web/hissafy/controller/ProContoller.java
+++ backend/hissafy/src/main/java/com/web/hissafy/controller/ProContoller.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ import com.web.hissafy.service.IProService;
 import com.web.hisssafy.dto.ProDto;
 
 import io.swagger.annotations.ApiOperation;
-
+@Controller
 public class ProContoller {
 
 	@Autowired
