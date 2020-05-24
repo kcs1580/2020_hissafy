08f9c11193c1d33a0b435f23f77799b246b82628
김창수 <kcs1580@naver.com>
2020-05-25 01:31:28 +0900

    S02P31C105-26[+]feat: 1인모드 레이아웃 구성

--- frontend/hi_ssafy/src/views/OneMode.vue
+++ frontend/hi_ssafy/src/views/OneMode.vue
@@ -2,46 +2,16 @@
   <div id="app">
     <v-app id="inspire">
       <v-app id="inspire">
-        <v-navigation-drawer v-model="drawer" app>
-          <v-list dense>
-            <v-list-item link>
-              <v-list-item-action>
-                <v-icon>mdi-home</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>Home</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item link>
-              <v-list-item-action>
-                <v-icon>mdi-contact-mail</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>Contact</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-
         <v-app-bar app color="indigo" dark>
-          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-          <v-toolbar-title>Application</v-toolbar-title>
+          <v-toolbar-title>1인 모드</v-toolbar-title>
         </v-app-bar>
 
         <v-content>
-          <v-container class="fill-height" fluid>
-            <v-row align="center" justify="center">
-              <v-col class="text-center">
-                <v-tooltip left>
-                  <template v-slot:activator="{ on }">
-                    <v-btn :href="source" icon large target="_blank" v-on="on">
-                      <v-icon large>mdi-code-tags</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
-              </v-col>
-            </v-row>
+          <v-container>
+            <v-layout row wrap align-center>
+              <v-flex xs12 md4>사용자인식</v-flex>
+              <v-flex xs12 md5 offset-md2>1인용 메뉴들</v-flex>
+            </v-layout>
           </v-container>
         </v-content>
         <v-footer color="indigo" app>
