869bdb2d56c336cd62d2f0dd3901cd9a4ba66946
김창수 <kcs1580@naver.com>
2020-06-07 15:37:17 +0900

    S02P31C105-26[+]fix: 1인모드 디자인 변경

--- frontend/hi_ssafy/src/views/OneMode.vue
+++ frontend/hi_ssafy/src/views/OneMode.vue
@@ -1,20 +1,35 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
-      <v-app-bar app color="indigo" dark>
+  
+    <v-app id="app" W>
+      <v-app-bar app color="#000839" dark>
         <v-toolbar-title>1인 모드</v-toolbar-title>
       </v-app-bar>
 
       <v-content id="menus">
-        <v-row justify="space-around">
+        <v-row >
           <v-col cols="12" sm="6">
-            <v-container class="grey lighten-5"></v-container>
+            <v-container ></v-container>
           </v-col>
           <v-col cols="12" sm="6">
-            <v-container class="grey lighten-5">
-              <v-dialog v-model="dialog" persistent max-width="600px">
+            <v-container >
+               <v-card
+    class="mx-auto"
+    max-width="150"
+  >
+    <v-card-text>
+      
+      <p class="display-1 text--primary">
+    <v-icon x-large color="blue darken-2" >mdi-home-export-outline</v-icon>
+      </p>
+   
+    </v-card-text>
+    <v-card-actions class="justify-center">
+       
+      <v-dialog v-model="dialog" persistent max-width="600px">
                 <template v-slot:activator="{ on }">
-                  <v-btn color="primary" dark v-on="on" large="true" class="earlybtn">조퇴/외출 신청</v-btn>
+                   
+                   
+                  <v-btn color="#005082" dark v-on="on"  x-large="true" >조퇴/외출 신청</v-btn>
                 </template>
                 <v-card>
                   <v-card-title>
@@ -93,29 +108,90 @@
                   </v-card-text>
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="dialog = false">취소</v-btn>
+                    <v-btn color="blue darken-1"  text @click="dialog = false">취소</v-btn>
                     <v-btn color="blue darken-1" text @click="submitForm">신청</v-btn>
                   </v-card-actions>
                 </v-card>
               </v-dialog>
-              <v-row>
-                <v-col cols="12" sm="6">
-                  <v-icon large color="blue darken-2">mdi-login</v-icon>
+    
+    </v-card-actions>
+  </v-card>
+              
+              <v-row >
+                <v-col cols="12" sm="2">
                 </v-col>
-                <v-col cols="12" sm="6">
-                  <v-icon large color="blue darken-2">mdi-logout</v-icon>
+                <v-col cols="12" sm="4">
+                  <v-card
+    class="mx-auto"
+    max-width="150"
+  >
+    <v-card-text>
+      
+      <p class="display-1 text--primary">
+        
+       <v-icon x-large color="blue darken-2" >mdi-login</v-icon>
+      
+      </p>
+   
+    </v-card-text>
+    <v-card-actions class="justify-center">
+      <div align="center">
+      <v-btn
+        dark
+        x-large
+        color="#005082"
+       @click="addAttendanceStudent"
+      >
+       입실하기
+      </v-btn>
+      </div>
+    </v-card-actions>
+  </v-card>
+
+                </v-col>
+              
+                <v-col cols="12" sm="4">
+                  <v-card
+    class="mx-auto"
+    max-width="150"
+  >
+    <v-card-text>
+      
+      <p class="display-1 text--primary">
+       <v-icon x-large color="blue darken-2" >mdi-logout</v-icon>
+      </p>
+   
+    </v-card-text>
+    <v-card-actions class="justify-center">
+       <div align="center">
+      <v-btn
+        dark
+        x-large
+      color="#005082"
+       @click="addAttendanceStudent"
+      >
+       퇴실하기
+      </v-btn>
+       </div>
+    </v-card-actions>
+  </v-card>
+                </v-col>
+                  <v-col cols="12" sm="2">
                 </v-col>
               </v-row>
+            
+  
+    
             </v-container>
           </v-col>
         </v-row>
       </v-content>
 
-      <v-footer color="indigo" app>
-        <span class="white--text">&copy; 2019</span>
+      <v-footer color="#000839" app>
+        <span class="white--text">&copy; 2020 하이바이싸피</span>
       </v-footer>
     </v-app>
-  </div>
+  
 </template>
 
 <script>
@@ -124,6 +200,7 @@ export default {
   name: "OneMode",
   data() {
     return {
+            studentList: [],
       faceid: "",
       dialog: false,
       modal2: false,
@@ -160,23 +237,93 @@ export default {
       this.dialog = false;
       this.outtime = null;
     },
+     addAttendanceStudent () {
+      let randomFaceId = String(this.getRandomIntInclusive(0,23))
+      let student = null
+      for(let i = 0 ; i < this.studentList.length ; i++) {
+        if(this.studentList[i].face_id === randomFaceId){
+            student = this.studentList[i];
+            break;
+        }
+      }
+      console.log(student)
+      if(student !== null && student.attendance_state === null) {
+        student.attendance_state = "입실완료"
+        student.attendance_time = new Date()
+        this.currentStudentList.unshift(student)
+        console.log(student.name + " " + student.attendance_state)
+        let fdata = new FormData()
+        fdata.append('student_id', student.student_id)
+        fdata.append('attendance_date', student.attendance_date)
+        fdata.append('attendance_time', student.attendance_time)
+        fdata.append('leaving_time', student.leaving_time)
+        fdata.append('attendance_state', student.attendance_state)
+        http
+          .put('/student/updateAttendance', fdata)
+          .then(
+            response => {
+              console.log(response.data.message)
+            }
+          )
+          .catch(err => console.log(err))
+          .finally(
+          )
+
+      } else if(student !== null && student.attendance_state === "입실완료") {
+        student.attendance_state = "퇴실완료"
+        student.leaving_time = new Date()
+        this.currentStudentList.unshift(student)
+        console.log(student.name + " " + student.attendance_state)
+        let fdata = new FormData()
+        fdata.append('student_id', student.student_id)
+        fdata.append('attendance_date', student.attendance_date)
+        fdata.append('attendance_time', student.attendance_time)
+        fdata.append('leaving_time', student.leaving_time)
+        fdata.append('attendance_state', student.attendance_state)
+        http
+          .put('/student/updateAttendance', fdata)
+          .then(
+            response => {
+              console.log(response.data.message)
+            }
+          )
+          .catch(err => console.log(err))
+          .finally(
+          )
+
+      } else if(student !== null) {
+        console.log(student.name + "은(는) 이미 입퇴실 처리 되었습니다.")
+      }
+    },
     login() {},
     logout() {}
   },
-  mounted() {},
+  mounted() {
+     http
+      .get('/student/studentduringlist')
+      .then(
+        response => {
+          console.log(response.data.message)
+          this.studentList = response.data.result;
+        }
+      )
+      .catch(err => console.log(err))
+      .finally(
+      )
+  },
 
   components: {}
 };
 </script>
 
 <style>
-#menus: {
-  height: 1000px;
+#menus {
+ 
 }
-body: {
-  background-color: #f0f0f0;
+#app {
+ background-color: #00a8cc;
 }
-.inbtn: {
+.inbtn {
   min-height: 500px;
   size: 500px;
 }
@@ -193,7 +340,7 @@ body: {
 }
 #app {
   text-align: center;
-  color: #2c3e50;
+  
   margin-top: 60px;
 }
 #video {
@@ -202,8 +349,5 @@ body: {
 #canvas {
   display: none;
 }
-li {
-  display: inline;
-  padding: 5px;
-}
+
 </style>
